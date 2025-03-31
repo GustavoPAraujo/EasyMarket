f760203068d79bf54f4cd7dd80c369ef7eea082f
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-31 11:30:39 -0300

    login route

--- backend/auth_services/src/controllers/auth.controllers.ts
+++ backend/auth_services/src/controllers/auth.controllers.ts
@@ -1,40 +1,81 @@
 
-  import { Request, Response } from "express"
-  import bcrypt from "bcrypt"
-  
-  import prisma from "../services/prisma";
-  
+import { Request, response, Response } from "express"
+import bcrypt from "bcrypt"
 
-  export const register = async (req: Request, res: Response): Promise<void> => {
+import prisma from "../services/prisma";
 
-    try {
-      const { name, email, password } = req.body;
 
-      if (!name || !email || !password) {
-        res.status(400).json({ message: "Missing required fields" });
-        return;
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
+
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    const newUser = await prisma.user.create({
+      data: { name, email, password: hashedPassword }
+    })
+
+    res.status(201).json({
+      message: "User created!",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email
       }
+    })
 
-      const saltRounds = 10
-      const hashedPassword = await bcrypt.hash(password, saltRounds)
+  } catch (err) {
+    console.error("Error registering user:", err)
 
+    if (!res.headersSent) res.status(500).json({ message: "Internal server error" });
+  }
+}
 
-      const newUser = await prisma.user.create({
-        data: { name, email, password: hashedPassword }
-      })
-  
-      res.status(201).json({
-        message: "User created!",
-        user: {
-          id: newUser.id,
-          name: newUser.name,
-          email: newUser.email
-        }
-      })
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
 
-    } catch (err) {
-      console.error("Error registering user:", err)
+    if (!email || !password) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
 
-      if (!res.headersSent) res.status(500).json({ message: "Internal server error" });
+    const user = await prisma.user.findFirst({
+      where: { email }
+    });
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      res.status(400).json({ message: "Invalid password" });
+      return;
+    }
+
+    res.status(200).json({
+      message: "User logged in successfully!",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    console.error("Error logging in user:", err);
+
+    if (!res.headersSent) {
+      res.status(500).json({ message: "Internal server error" });
     }
   }
+}
--- backend/auth_services/src/routes/auth.routes.ts
+++ backend/auth_services/src/routes/auth.routes.ts
@@ -5,10 +5,8 @@ import * as authController from "../controllers/auth.controllers"
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Easy Market - Auth Service')
-})
 
 router.post('/register', authController.register)
+router.get('/login', authController.login)
 
 export default router
