ce0d254e340f68ae3de1144427f62937af7cfe68
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-31 10:36:45 -0300

    connection to pgAdmin

--- backend/auth_services/src/controllers/auth.controllers.ts
+++ backend/auth_services/src/controllers/auth.controllers.ts
@@ -1,6 +1,6 @@
 
   import { Request, Response } from "express"
-  import { userInfo } from "os"
+  import prisma from "../services/prisma";
 
   export const register = async (req: Request, res: Response): Promise<void> => {
 
@@ -11,11 +11,25 @@
         res.status(400).json({ message: "Missing required fields" });
         return;
       }
+
+      const newUser = await prisma.user.create({
+        data: { name, email, password }
+      })
+  
+      res.status(201).json({
+        message: "User created!",
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email
+        }
+      })
+
       res.status(200).json({user: {name, email}, message: "User info"})
 
     } catch (err) {
       console.error("Error registering user:", err)
       res.status(500).json({ message: "Internal server error" });
     }
-
   }
+  
