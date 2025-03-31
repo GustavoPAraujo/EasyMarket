b9e2a102f27ef20ebce9ffa75aade570e1a3b398
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-31 13:13:58 -0300

    file names and imports

--- backend/auth_services/src/routes/auth.routes.ts
+++ backend/auth_services/src/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 
 import express from "express";
 
-import * as authController from "../controllers/auth.controllers"
+import * as authController from "../controllers/auth.controller"
 
 const router = express.Router()
 
--- backend/auth_services/src/routes/user.routes.ts
+++ backend/auth_services/src/routes/user.routes.ts
@@ -1,11 +1,11 @@
 
 import { Router } from "express"
-import { getMe } from "../controllers/user.controller"
+import * as userController from "../controllers/user.controller"
 import { authenticateToken } from "../middlewares/auth.middleware";
 
 
 const router = Router();
 
-router.get("/me", authenticateToken, getMe);
+router.get("/me", authenticateToken, userController.getMe);
 
 export default router;
