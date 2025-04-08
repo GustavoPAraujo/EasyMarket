258b6f8697b5a09c8566295a7d8e42c4bf6f2ddf
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-08 12:47:49 -0300

    create products route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 import authRoutes from "./auth/auth.routes"
 import userRoutes from './users/user.routes';
 import storeRoutes from './stores/store.routes';
+import productRouter from './products/product.routes'
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/store", storeRoutes)
+app.use("/api/product", productRouter)
 
 
 app.listen(port, () => {
