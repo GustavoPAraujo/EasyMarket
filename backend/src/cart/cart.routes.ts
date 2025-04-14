28021b2efa9bb7ab6c62d65ad6d6bc3265f01cc5
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-13 22:18:08 -0300

    checkout route

--- backend/src/cart/cart.routes.ts
+++ backend/src/cart/cart.routes.ts
@@ -6,6 +6,7 @@ import * as cartController from "./cart.controller"
 const router = express.Router()
 
 router.post("/items", authenticateToken, cartController.addItemToCart )
+router.post("/checkout", authenticateToken, cartController.checkout )
 router.get("/", authenticateToken, cartController.getCart )
 router.get("/summary", authenticateToken, cartController.getCartSummary )
 router.patch("/:itemId", authenticateToken, cartController.updateCartItem )
