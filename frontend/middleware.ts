68d1a4aad900defb3ce7b6ab3740738d83ef5b90
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-19 11:09:48 -0300

    auth middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,76 @@
+
+import { NextResponse } from "next/server";
+import type { NextRequest, MiddlewareConfig } from "next/server";
+import { jwtVerify } from "jose";
+
+const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET!);
+
+const publicRoutes = ["/", "/auth"];
+const protectedRoutes = [""];
+const adminRoutes = [""];
+
+function match(pathname: string, patterns: string[]) {
+  return patterns.some(pattern => {
+    const regex = new RegExp("^" +
+      pattern.replace(/:\w+/g, "[^/]+")
+             .replace(/\*/g, ".*")
+      + "$");
+    return regex.test(pathname);
+  });
+}
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get("token")?.value;
+
+
+  if (match(pathname, publicRoutes)) {
+    if (token && ["/auth","/auth"].includes(pathname)) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (match(pathname, protectedRoutes)) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/lauthogin", request.url));
+    }
+
+    try {
+      await jwtVerify(token, jwtSecret);
+      return NextResponse.next();
+    } catch {
+
+      const res = NextResponse.redirect(new URL("/login", request.url));
+      res.cookies.delete("token");
+      return res;
+    }
+  }
+
+
+  if (match(pathname, adminRoutes)) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+    try {
+      const { payload }: any = await jwtVerify(token, jwtSecret);
+      if (payload.role !== "ADMIN") {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      return NextResponse.next();
+    } catch {
+      const res = NextResponse.redirect(new URL("/auth", request.url));
+      res.cookies.delete("token");
+      return res;
+    }
+  }
+
+  //tratar para 404
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
