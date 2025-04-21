943977e794a182568e2f5f58318193d3a9f5ae47
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-21 18:35:26 -0300

    hide adminSidebar when creating store

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,15 +1,22 @@
+"use client"
 
 import { StoreProvider } from "./storeContext";
 import AdminSidebar from "@/components/sidebar/sidebar"
+import { usePathname } from "next/navigation"
 
 type Props = { children: React.ReactNode };
 
 
 export default function AdminLayout({ children }: Props) {
+
+  const pathname = usePathname()
+
+
+
   return (
     <StoreProvider>
       <div className="flex min-h-screen">
-        <AdminSidebar />
+        { pathname === "/admin/store/create" ? <></> : <AdminSidebar /> }
         <main className="flex-1 p-8">{children}</main>
       </div>
     </StoreProvider>
