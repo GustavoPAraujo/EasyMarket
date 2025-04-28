12c3c43f5f2cb7fddaf702be0f0baa2103a7e8e8
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-28 17:48:09 -0300

    update product service

--- frontend/services/productServices.ts
+++ frontend/services/productServices.ts
@@ -13,10 +13,25 @@ export const createProduct = async (adminId: number, productData: createProduct)
 
   try {
     const response = await api.post("/product", productData)
-
     return response.data
      
   } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateProduct = async ( 
+  productId: number,
+  updateData: { name?: string, description?: string, quantity?: number, price?: number, categoryId?: number} 
+) =>{
+
+  try {
+    const response = await api.post(`/product/${productId}`, updateData)
+    console.log("response: ", response)
+    return response.data
+
+
+  } catch(err) {
     console.error(err)
   }
 
